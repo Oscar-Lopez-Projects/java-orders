ad47791b727b5252b3f153128fb04cd721d360d7
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-10 15:38:11 -0700

    setup agent class from models

--- src/main/java/com/lambda/javaorder/models/Agent.java
+++ src/main/java/com/lambda/javaorder/models/Agent.java
@@ -7,7 +7,74 @@ import javax.persistence.*;
 public class Agent {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long agentid;
+    private long agentcode;
 
-    
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    private String workingarea;
+    private double commission;
+    private String phone;
+    private String country;
+
+    public Agent() {
+    }
+
+    //constructor of all data
+    public Agent(String name, String workingarea, double commission, String phone, String country) {
+        this.name = name;
+        this.workingarea = workingarea;
+        this.commission = commission;
+        this.phone = phone;
+        this.country = country;
+    }
+    //now all of getter and setters
+
+    public long getAgentcode() {
+        return agentcode;
+    }
+
+    public void setAgentcode(long agentcode) {
+        this.agentcode = agentcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorkingarea() {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea) {
+        this.workingarea = workingarea;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
