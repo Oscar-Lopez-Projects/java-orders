e5d272be3946e6d815a5292267ac127027c2a50f
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-08-12 16:17:11 -0700

    second get is working now

--- src/main/java/com/lambdaschool/crudyorders/controllers/CustomerController.java
+++ src/main/java/com/lambdaschool/crudyorders/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,12 +18,28 @@ public class CustomerController {
     @Autowired
     private CustomerService customerServices;
 
-    // // http://localhost:2019/customers/orders
+    // http://localhost:2019/customers/orders
     @GetMapping(value = "orders", produces = "application/json")
     public ResponseEntity<?> listAllCustomers(){
         List<Customer> myList= customerServices.listAllCustomers();
         return new ResponseEntity<>(myList, HttpStatus.OK);
     }
+
+    //http://localhost:2019/customers/customer/7
+
+    @GetMapping(value = "/customer/{custcode}",produces = "application/json")
+    public ResponseEntity<?> findCustomerById(@PathVariable long custcode){
+        Customer myCustomer = customerServices.findCustomersByCode(custcode);
+        return new ResponseEntity<>(myCustomer,HttpStatus.OK);
+    }
+
+    //http://localhost:2019/customers/customer/77
+
+    //http://localhost:2019/customers/namelike/mes
+
+    //http://localhost:2019/customers/namelike/cin
+
+
 
 
 }
